365bfe6df747dd42c5d347a67ae8d74d5cf2854d
Nandhakishor Yadav <111010269+nandhakishoryadav@users.noreply.github.com>
2023-08-16 13:58:54 +0530

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using ConsoleApp4;
+
+utility ut1=new utility();
+
+List<int> list1 = new List<int>();
+list1.Add(100);
+list1.Add(200);
+list1.Add(300);
+list1.Add(400);
+ut1.DisplayListContents(list1);
+
+List < double> list2 = new List<double>();
+list2.Add(1.2);
+list2.Add(2.3);
+list2.Add(3.2);
+list2.Add(4.3);
+ut1.DisplayListContents(list2);
+
+List<double> list3 = new List<double>();
+list2.Add(1.2);
+list2.Add(2.3);
+list2.Add(3.2);
+list2.Add(4.3);
+ut1.DisplayListContents(list2);
---
+++ ConsoleApp4/ConsoleApp4/utility.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class utility
+    {
+        public T Add<T>(T a, T b)
+        {
+            return(dynamic)a + (dynamic)b;
+        }
+
+        public void DisplayListContents(List<int>listOfInt)
+        {
+            foreach(var item in listOfInt)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void DisplayListContents(List<double>listOfDouble) 
+        {
+            foreach (var item in listOfDouble) 
+            { 
+                Console.WriteLine(item);
+            }
+        }
+
+        public void DisplayListContents<T>(List<T>list)
+        {
+            foreach (var item in list) 
+            {
+            Console.WriteLine(item);
+            }
+        }
+    }
+}
