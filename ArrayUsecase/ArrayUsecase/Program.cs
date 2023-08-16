92f4a31e29b0431d778dc1de2cbd338e905fe62d
Nandhakishor Yadav <111010269+nandhakishoryadav@users.noreply.github.com>
2023-08-16 13:53:56 +0530

    Add files via upload

---
+++ ArrayUsecase/ArrayUsecase/Program.cs
@@ -0,0 +1,7 @@
+﻿// program in C# Sharp to store elements in an array and print it
+
+int[] arrval1= new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+for (int i = 0; i < arrval1.Length; i++) 
+{
+    Console.WriteLine(arrval1[i]);
+}
